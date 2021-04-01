45c3cee3f75485dd61ed39a540e7040ea17733c8
admin <admin@192.168.219.103>
2021-04-02 00:55:15 +0900

    test4

--- src/forExam/test3.java
+++ src/forExam/test3.java
@@ -3,6 +3,6 @@ package forExam;
 public class test3 {
 	public static void main(String[] args) {
 		System.out.println("test3");
-		System.out.println("test3");
+		System.out.println("test33");
 	}
 }
