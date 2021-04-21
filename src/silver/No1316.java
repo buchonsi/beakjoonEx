d59784f6ae08e062b8c3e9563b238779d1acd9bf
buchonsi <buchonsi@gmail.com>
2021-04-22 00:43:37 +0900

    HARD - 다시

---
+++ src/silver/No1316.java
@@ -0,0 +1,42 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No1316 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		int count = Integer.parseInt(br.readLine());
+		int group =0;
+		for(int i=0; i<count; i++) {
+			if(group()==true) {
+				group++;
+			}
+		}
+		System.out.println(group);
+		
+	}
+	
+	private static boolean group() throws IOException {
+		boolean[] alp = new boolean[26];
+		String s = br.readLine();
+		int oldNum = 0;
+		
+		for(int i=0; i<s.length(); i++) {
+			int newNum = s.charAt(i);
+			
+			if(oldNum != newNum) {
+				if(alp[newNum-'a']==false) {
+					alp[newNum-'a'] = true;
+					oldNum = newNum;
+				}else {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
